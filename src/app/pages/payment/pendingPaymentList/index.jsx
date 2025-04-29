371f3a5edf7e109a2266b054dda2d9b2babbd49b
luckyboy2790 <rorifloris78@gmail.com>
2025-04-29 08:21:02 +0100

    fix: update payment icon

--- src/app/pages/payment/pendingPaymentList/index.jsx
+++ src/app/pages/payment/pendingPaymentList/index.jsx
@@ -1,6 +1,6 @@
 import { Page } from "components/shared/Page";
 import { Breadcrumbs } from "components/shared/Breadcrumbs";
-import PurchaseIcon from "assets/dualicons/purchase.svg?react";
+import PaymentIcon from "assets/dualicons/payments.svg?react";
 import { useTranslation } from "react-i18next";
 import PurchaseTable from "./purchaseTable";
 
@@ -17,7 +17,7 @@ const PurchaseList = () => {
         <div className="flex min-w-0 flex-col gap-8">
           <div className="flex items-center justify-between gap-4">
             <div className="flex items-center gap-2">
-              <PurchaseIcon className="size-6 shrink-0 stroke-[1.5]" />
+              <PaymentIcon className="size-6 shrink-0 stroke-[1.5]" />
               <h2 className="dark:text-dark-50 truncate text-xl font-medium tracking-wide text-gray-800">
                 {t("nav.payment.payment")}
               </h2>
