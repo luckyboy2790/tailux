6fe6cc470bff0fb963332c6f2060c9b4826745e1
luckyboy2790 <rorifloris78@gmail.com>
2025-05-12 19:23:47 +0100

    feat: complete expired purchases report table page

---
+++ src/app/pages/reports/expiredPurchaseReport/dataTable/Toolbar.jsx
@@ -0,0 +1,247 @@
+// Import Dependencies
+import {
+  ChevronUpDownIcon,
+  MagnifyingGlassIcon,
+} from "@heroicons/react/24/outline";
+import { TbUpload } from "react-icons/tb";
+import clsx from "clsx";
+import {
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+  Transition,
+} from "@headlessui/react";
+import { EllipsisHorizontalIcon } from "@heroicons/react/20/solid";
+import PropTypes from "prop-types";
+
+// Local Imports
+import { Button, Input } from "components/ui";
+import { TableConfig } from "./TableConfig";
+import { useBreakpointsContext } from "app/contexts/breakpoint/context";
+
+export function Toolbar({ table }) {
+  const { isXs } = useBreakpointsContext();
+  const isFullScreenEnabled = table.getState().tableSettings.enableFullScreen;
+
+  return (
+    <div className="table-toolbar">
+      <div
+        className={clsx(
+          "transition-content flex items-center justify-end gap-4",
+          isFullScreenEnabled ? "px-4 sm:px-5" : "px-(--margin-x) pt-4",
+        )}
+      >
+        {isXs ? (
+          <Menu as="div" className="relative inline-block text-left">
+            <MenuButton
+              as={Button}
+              variant="flat"
+              className="size-8 shrink-0 rounded-full p-0"
+            >
+              <EllipsisHorizontalIcon className="size-4.5" />
+            </MenuButton>
+            <Transition
+              as={MenuItems}
+              enter="transition ease-out"
+              enterFrom="opacity-0 translate-y-2"
+              enterTo="opacity-100 translate-y-0"
+              leave="transition ease-in"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 translate-y-2"
+              className="dark:border-dark-500 dark:bg-dark-700 absolute z-100 mt-1.5 min-w-[10rem] rounded-lg border border-gray-300 bg-white py-1 whitespace-nowrap shadow-lg shadow-gray-200/50 outline-hidden focus-visible:outline-hidden ltr:right-0 rtl:left-0 dark:shadow-none"
+            >
+              <MenuItem>
+                {({ focus }) => (
+                  <button
+                    className={clsx(
+                      "flex h-9 w-full items-center px-3 tracking-wide outline-hidden transition-colors",
+                      focus &&
+                        "dark:bg-dark-600 dark:text-dark-100 bg-gray-100 text-gray-800",
+                    )}
+                  >
+                    <span>New Order</span>
+                  </button>
+                )}
+              </MenuItem>
+              <hr className="border-gray-150 dark:border-dark-500 mx-3 my-1.5 h-px" />
+              <MenuItem>
+                {({ focus }) => (
+                  <button
+                    className={clsx(
+                      "flex h-9 w-full items-center px-3 tracking-wide outline-hidden transition-colors",
+                      focus &&
+                        "dark:bg-dark-600 dark:text-dark-100 bg-gray-100 text-gray-800",
+                    )}
+                  >
+                    <span>Export as PDF</span>
+                  </button>
+                )}
+              </MenuItem>
+              <MenuItem>
+                {({ focus }) => (
+                  <button
+                    className={clsx(
+                      "flex h-9 w-full items-center px-3 tracking-wide outline-hidden transition-colors",
+                      focus &&
+                        "dark:bg-dark-600 dark:text-dark-100 bg-gray-100 text-gray-800",
+                    )}
+                  >
+                    <span>Export as CSV</span>
+                  </button>
+                )}
+              </MenuItem>
+            </Transition>
+          </Menu>
+        ) : (
+          <div className="flex space-x-2">
+            <Menu
+              as="div"
+              className="relative inline-block text-left whitespace-nowrap"
+            >
+              <MenuButton
+                as={Button}
+                variant="outlined"
+                className="h-8 space-x-2 rounded-md px-3 text-xs"
+              >
+                <TbUpload className="size-4" />
+                <span>Export</span>
+                <ChevronUpDownIcon className="size-4" />
+              </MenuButton>
+              <Transition
+                as={MenuItems}
+                enter="transition ease-out"
+                enterFrom="opacity-0 translate-y-2"
+                enterTo="opacity-100 translate-y-0"
+                leave="transition ease-in"
+                leaveFrom="opacity-100 translate-y-0"
+                leaveTo="opacity-0 translate-y-2"
+                className="dark:border-dark-500 dark:bg-dark-700 absolute z-100 mt-1.5 min-w-[10rem] rounded-lg border border-gray-300 bg-white py-1 shadow-lg shadow-gray-200/50 outline-hidden focus-visible:outline-hidden ltr:right-0 rtl:left-0 dark:shadow-none"
+              >
+                <MenuItem>
+                  {({ focus }) => (
+                    <button
+                      className={clsx(
+                        "flex h-9 w-full items-center px-3 tracking-wide outline-hidden transition-colors",
+                        focus &&
+                          "dark:bg-dark-600 dark:text-dark-100 bg-gray-100 text-gray-800",
+                      )}
+                    >
+                      <span>Export as PDF</span>
+                    </button>
+                  )}
+                </MenuItem>
+                <MenuItem>
+                  {({ focus }) => (
+                    <button
+                      className={clsx(
+                        "flex h-9 w-full items-center px-3 tracking-wide outline-hidden transition-colors",
+                        focus &&
+                          "dark:bg-dark-600 dark:text-dark-100 bg-gray-100 text-gray-800",
+                      )}
+                    >
+                      <span>Export as CSV</span>
+                    </button>
+                  )}
+                </MenuItem>
+              </Transition>
+            </Menu>
+
+            <Menu
+              as="div"
+              className="relative inline-block text-left whitespace-nowrap"
+            >
+              <MenuButton
+                as={Button}
+                variant="outlined"
+                className="h-8 shrink-0 rounded-md px-2.5"
+              >
+                <EllipsisHorizontalIcon className="size-4.5" />
+              </MenuButton>
+              <Transition
+                as={MenuItems}
+                enter="transition ease-out"
+                enterFrom="opacity-0 translate-y-2"
+                enterTo="opacity-100 translate-y-0"
+                leave="transition ease-in"
+                leaveFrom="opacity-100 translate-y-0"
+                leaveTo="opacity-0 translate-y-2"
+                className="dark:border-dark-500 dark:bg-dark-700 absolute z-100 mt-1.5 min-w-[10rem] rounded-lg border border-gray-300 bg-white py-1 whitespace-nowrap shadow-lg shadow-gray-200/50 outline-hidden focus-visible:outline-hidden ltr:right-0 rtl:left-0 dark:shadow-none"
+              >
+                <MenuItem>
+                  {({ focus }) => (
+                    <button
+                      className={clsx(
+                        "flex h-9 w-full items-center px-3 tracking-wide outline-hidden transition-colors",
+                        focus &&
+                          "dark:bg-dark-600 dark:text-dark-100 bg-gray-100 text-gray-800",
+                      )}
+                    >
+                      <span>New Order</span>
+                    </button>
+                  )}
+                </MenuItem>
+              </Transition>
+            </Menu>
+          </div>
+        )}
+      </div>
+
+      {isXs ? (
+        <>
+          <div
+            className={clsx(
+              "flex space-x-2 pt-4 [&_.input-root]:flex-1",
+              isFullScreenEnabled ? "px-4 sm:px-5" : "px-(--margin-x)",
+            )}
+          >
+            <SearchInput table={table} />
+            <TableConfig table={table} />
+          </div>
+        </>
+      ) : (
+        <div
+          className={clsx(
+            "custom-scrollbar transition-content flex justify-between space-x-4 overflow-x-auto pt-4 pb-1",
+            isFullScreenEnabled ? "px-4 sm:px-5" : "px-(--margin-x)",
+          )}
+          style={{
+            "--margin-scroll": isFullScreenEnabled
+              ? "1.25rem"
+              : "var(--margin-x)",
+          }}
+        >
+          <div className="flex shrink-0 space-x-2">
+            <SearchInput table={table} />
+          </div>
+
+          <TableConfig table={table} />
+        </div>
+      )}
+    </div>
+  );
+}
+
+function SearchInput({ table }) {
+  return (
+    <Input
+      value={table.getState().globalFilter}
+      onChange={(e) => table.setGlobalFilter(e.target.value)}
+      prefix={<MagnifyingGlassIcon className="size-4" />}
+      classNames={{
+        input: "ring-primary-500/50 h-8 text-xs focus:ring-3",
+        root: "shrink-0",
+      }}
+      placeholder="Search ID, Customer..."
+    />
+  );
+}
+
+Toolbar.propTypes = {
+  table: PropTypes.object.isRequired,
+  onDateRangeChange: PropTypes.func.isRequired,
+};
+
+SearchInput.propTypes = {
+  table: PropTypes.object.isRequired,
+};
--- src/app/pages/reports/expiredPurchaseReport/dataTable/columns.jsx
+++ src/app/pages/reports/expiredPurchaseReport/dataTable/columns.jsx
@@ -1,12 +1,5 @@
 // Import Dependencies
 import { createColumnHelper } from "@tanstack/react-table";
-
-// Local Imports
-// import { RowActions } from "./RowActions";
-import {
-  SelectCell,
-  SelectHeader,
-} from "components/shared/table/SelectCheckbox";
 import {
   //   AddressCell,
   CustomerCell,
@@ -14,7 +7,7 @@ import {
   //   OrderIdCell,
   OrderStatusCell,
   ProfitCell,
-  // TotalCell,
+  TotalCell,
 } from "./rows";
 
 // ----------------------------------------------------------------------
@@ -22,92 +15,108 @@ import {
 const columnHelper = createColumnHelper();
 
 export const columns = [
-  columnHelper.display({
-    id: "select",
-    label: "Row Selection",
-    header: SelectHeader,
-    cell: SelectCell,
-  }),
-  columnHelper.accessor((row) => Number(row.created_at), {
-    id: "date",
-    label: "Date",
+  columnHelper.accessor((row) => row?.timestamp, {
+    id: "timestamp",
+    label: "Order Date",
     header: "Date",
     cell: DateCell,
+    filterFn: "inNumberRange",
+    enableSorting: true,
   }),
-  columnHelper.accessor((row) => Number(row.created_at), {
+  columnHelper.accessor((row) => row?.expiry_date, {
     id: "expiry_date",
     label: "Expiry Date",
     header: "Expiry Date",
     cell: DateCell,
+    filterFn: "inNumberRange",
+    enableSorting: true,
   }),
-  columnHelper.accessor((row) => row.customer.name, {
+  columnHelper.accessor((row) => row?.reference_no, {
     id: "reference_no",
     label: "Reference No",
     header: "Reference No",
-    cell: CustomerCell,
+    cell: (props) => {
+      return (
+        <p className="text-sm-plus dark:text-dark-100 font-medium text-gray-800">
+          {props.row.original?.reference_no}
+        </p>
+      );
+    },
+    enableSorting: false,
   }),
-  columnHelper.accessor((row) => row.customer.name, {
+  columnHelper.accessor((row) => row?.company.name, {
     id: "company",
     label: "Company",
     header: "Company",
-    cell: () => (
-      <div className="dark:text-dark-100 flex items-center gap-2 text-gray-800">
-        <span className="text-sm-plus font-medium">1</span>
-      </div>
-    ),
+    cell: CustomerCell,
+    enableSorting: false,
   }),
-  columnHelper.accessor((row) => row.customer.name, {
+  columnHelper.accessor((row) => row?.store.name, {
     id: "store",
     label: "Store",
     header: "Store",
-    cell: () => (
-      <div className="dark:text-dark-100 flex items-center gap-2 text-gray-800">
-        <span className="text-sm-plus font-medium">1</span>
-      </div>
-    ),
+    cell: CustomerCell,
+    enableSorting: false,
   }),
-  columnHelper.accessor((row) => row.customer.name, {
+  columnHelper.accessor((row) => row?.supplier.company, {
     id: "supplier",
     label: "Supplier",
     header: "Supplier",
     cell: CustomerCell,
+    enableSorting: false,
   }),
-  columnHelper.accessor((row) => row.customer.name, {
-    id: "product",
-    label: "Product(Qty)",
-    header: "Product(Qty)",
-    cell: () => (
-      <div className="dark:text-dark-100 flex items-center gap-2 text-gray-800">
-        <span className="text-sm-plus font-medium">1</span>
-      </div>
-    ),
-  }),
-  columnHelper.accessor((row) => row.profit, {
+  columnHelper.accessor((row) => row?.total_amount, {
     id: "grand_total",
     label: "Grand Total",
     header: "Grand Total",
-    cell: ProfitCell,
+    cell: TotalCell,
     filterFn: "inNumberRange",
+    enableSorting: false,
   }),
-  columnHelper.accessor((row) => row.profit, {
+  columnHelper.accessor((row) => row?.paid_amount, {
     id: "paid",
     label: "Paid",
     header: "Paid",
     cell: ProfitCell,
     filterFn: "inNumberRange",
+    enableSorting: false,
   }),
-  columnHelper.accessor((row) => row.profit, {
+  columnHelper.accessor((row) => row?.grand_total - row?.paid_amount, {
     id: "balance",
     label: "Balance",
     header: "Balance",
-    cell: ProfitCell,
+    cell: (props) => (
+      <p
+        className={`text-sm-plus ${props.row.original?.grand_total < props.row.original?.paid_amount ? "dark:text-red-500" : "dark:text-dark-100"} font-medium text-gray-800`}
+      >
+        $
+        {(
+          (props.row.original?.grand_total || 0) -
+          (props.row.original?.paid_amount || 0)
+        ).toLocaleString(undefined, {
+          minimumFractionDigits: 2,
+          maximumFractionDigits: 2,
+        })}
+      </p>
+    ),
     filterFn: "inNumberRange",
+    enableSorting: false,
   }),
-  columnHelper.accessor((row) => row.order_status, {
-    id: "payment_status",
-    label: "Payment Status",
-    header: "Payment Status",
+  columnHelper.accessor((row) => row, {
+    id: "order_status",
+    label: "Order Status",
+    header: "Order Status",
     cell: OrderStatusCell,
     filterFn: "arrIncludesSome",
+    enableSorting: false,
   }),
+  //   columnHelper.accessor(
+  //     (row) => `${row.shipping_address?.street}, ${row.shipping_address?.line}`,
+  //     {
+  //       id: "address",
+  //       label: "Address",
+  //       header: "Address",
+  //       cell: AddressCell,
+  //     },
+  //   ),
 ];
