3a73b583f74ad510f2123f7bb4e975a2f26e7e83
luckyboy2790 <rorifloris78@gmail.com>
2025-05-13 01:17:54 +0100

    feat: complete users report table page

--- src/app/pages/reports/usersReport/dataTable/columns.jsx
+++ src/app/pages/reports/usersReport/dataTable/columns.jsx
@@ -3,109 +3,108 @@ import { createColumnHelper } from "@tanstack/react-table";
 
 // Local Imports
 import { RowActions } from "./RowActions";
-import {
-  SelectCell,
-  SelectHeader,
-} from "components/shared/table/SelectCheckbox";
-import {
-  //   AddressCell,
-  // CustomerCell,
-  // DateCell,
-  //   OrderIdCell,
-  OrderStatusCell,
-  // ProfitCell,
-  // TotalCell,
-} from "./rows";
+import { OrderStatusCell } from "./rows";
 
 // ----------------------------------------------------------------------
 
 const columnHelper = createColumnHelper();
 
 export const columns = [
-  columnHelper.display({
-    id: "select",
-    label: "Row Selection",
-    header: SelectHeader,
-    cell: SelectCell,
-  }),
-  columnHelper.accessor((row) => Number(row.created_at), {
+  columnHelper.accessor((row) => row?.username, {
     id: "username",
     label: "Username",
     header: "Username",
-    cell: () => {
+    cell: (props) => {
       return (
-        <div className="dark:text-dark-100 flex items-center gap-2 text-gray-800">
-          <span className="text-sm-plus font-medium">aym</span>
-        </div>
+        <p className="text-sm-plus dark:text-dark-100 font-medium text-gray-800">
+          {props.row.original?.username}
+        </p>
       );
     },
+    enableSorting: false,
   }),
-  columnHelper.accessor((row) => Number(row.created_at), {
+  columnHelper.accessor((row) => row?.first_name, {
     id: "first_name",
     label: "First Name",
     header: "First Name",
-    cell: () => {
+    cell: (props) => {
       return (
-        <div className="dark:text-dark-100 flex items-center gap-2 text-gray-800">
-          <span className="text-sm-plus font-medium">aym</span>
-        </div>
+        <p className="text-sm-plus dark:text-dark-100 font-medium text-gray-800">
+          {props.row.original?.first_name}
+        </p>
       );
     },
+    enableSorting: false,
   }),
-  columnHelper.accessor((row) => Number(row.created_at), {
+  columnHelper.accessor((row) => row?.first_name, {
     id: "last_name",
     label: "Last Name",
     header: "Last Name",
-    cell: () => {
+    cell: (props) => {
       return (
-        <div className="dark:text-dark-100 flex items-center gap-2 text-gray-800">
-          <span className="text-sm-plus font-medium">aym</span>
-        </div>
+        <p className="text-sm-plus dark:text-dark-100 font-medium text-gray-800">
+          {props.row.original?.last_name}
+        </p>
       );
     },
+    enableSorting: false,
   }),
-  columnHelper.accessor((row) => Number(row.created_at), {
-    id: "company",
-    label: "Company",
-    header: "Company",
-    cell: () => {
+  columnHelper.accessor((row) => row?.company?.name, {
+    id: "company_name",
+    label: "Company Name",
+    header: "Company Name",
+    cell: (props) => {
       return (
-        <div className="dark:text-dark-100 flex items-center gap-2 text-gray-800">
-          <span className="text-sm-plus font-medium">aym</span>
-        </div>
+        <p className="text-sm-plus dark:text-dark-100 font-medium text-gray-800">
+          {props.row.original?.company?.name}
+        </p>
       );
     },
+    enableSorting: false,
   }),
-  columnHelper.accessor((row) => Number(row.created_at), {
+  columnHelper.accessor((row) => row?.phone_number, {
     id: "phone_number",
-    label: "Phone Number",
-    header: "Phone Number",
-    cell: () => {
+    label: "Phone number",
+    header: "Phone number",
+    cell: (props) => {
       return (
-        <div className="dark:text-dark-100 flex items-center gap-2 text-gray-800">
-          <span className="text-sm-plus font-medium">no</span>
-        </div>
+        <p className="text-sm-plus dark:text-dark-100 font-medium text-gray-800">
+          {props.row.original?.phone_number}
+        </p>
       );
     },
+    enableSorting: false,
   }),
-  columnHelper.accessor((row) => Number(row.created_at), {
+  columnHelper.accessor((row) => row?.role, {
     id: "role",
     label: "Role",
     header: "Role",
-    cell: () => {
+    cell: (props) => {
       return (
-        <div className="dark:text-dark-100 flex items-center gap-2 text-gray-800">
-          <span className="text-sm-plus font-medium">secretary</span>
-        </div>
+        <p className="text-sm-plus dark:text-dark-100 font-medium text-gray-800 capitalize">
+          {props.row.original?.role}
+        </p>
       );
     },
+    enableSorting: false,
   }),
-  columnHelper.accessor((row) => row.order_status, {
-    id: "status",
-    label: "Status",
-    header: "Status",
+  columnHelper.accessor((row) => row, {
+    id: "total_sales",
+    label: "Total Sales",
+    header: "Total Sales",
     cell: OrderStatusCell,
+    filterFn: "inNumberRange",
+    enableSorting: false,
   }),
+  //   columnHelper.accessor(
+  //     (row) => `${row.shipping_address?.street}, ${row.shipping_address?.line}`,
+  //     {
+  //       id: "address",
+  //       label: "Address",
+  //       header: "Address",
+  //       cell: AddressCell,
+  //     },
+  //   ),
   columnHelper.display({
     id: "actions",
     label: "Row Actions",
